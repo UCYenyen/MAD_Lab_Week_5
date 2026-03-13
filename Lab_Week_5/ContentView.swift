c5d48cdca244e56c94bcd97b5c1d74bc0febb57e
Bryan Fernando Dinata <bfernando@student.ciputra.ac.id>
2026-03-13 07:38:35 +0700

    Initial Commit

---
+++ Lab_Week_5/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lab_Week_5
+//
+//  Created by Bryan Fernando Dinata on 13/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lab_Week_5/Lab_Week_5App.swift
@@ -0,0 +1,17 @@
+//
+//  Lab_Week_5App.swift
+//  Lab_Week_5
+//
+//  Created by Bryan Fernando Dinata on 13/03/26.
+//
+
+import SwiftUI
+
+@main
+struct Lab_Week_5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
